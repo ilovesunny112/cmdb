438664dc24e3aa4feaf8f96f4624c9f428d407eb
sunny112 <9163524@qq.com>
2019-07-16 08:37:34 +0800

    test

--- thread/timer.py
+++ thread/timer.py
@@ -1,18 +1,34 @@
 
 import threading
 import time
+import logging
+
+FORMAT = "%(asctime)s %(thread)d %(message)s %(schoolname)s"
+
+d = {"schoolname" : "HEBUT"}
+
+logging.basicConfig(level=logging.INFO, format=FORMAT, datefmt="%Y-%m-%d-%H:%M:%S", filename="d:\\test.log", filemode="a" )
 
 def add(x, y):
-    print(x + y)
+    logging.info("{} {}".format(threading.enumerate(), x + y), extra=d)
 
-t = threading.Timer(5, add, args=(4,5) )
+t = threading.Timer(5, add, args=(4,5),)
+
+
 
 t.start()
 
 print(threading.enumerate())
-time.sleep(2)
-t.cancel()
+# time.sleep(2)
+# t.cancel()
 time.sleep(0.0001)
-
+logging.warning("警告警告", extra=d)
+logging.debug("调试信息", extra=d)
+# logging.info("%(module)s")
 
 print(threading.enumerate())
+log = logging.getLogger("a.b")
+
+print(log.name)
+print(log, type(log))
+log.info("aaa", extra = d)
